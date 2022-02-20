5c340c04d116681285c3d308f096072e20dc960a
KarinaStabile <karina.stabile@gmail.com>
2022-02-19 21:23:19 -0500

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+     {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
+//If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+//If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,22 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
-
 function App() {
+  
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
 
 export default App;
+
+//pass down data from the user object as props to the Home component so that it renders correctly. 
+
+//App should also pass down a prop of bio to the About component.
+
+//pass down the github and linkedin links to the About component, so that About can pass those props down to the Links component you will create.
